7822e2ce3fd2f2254ac142886517ba3430df1e39
jasim0021 <skjasimuddin9064@gmail.com>
2024-09-15 18:10:51 +0530

    video player in swiftui

--- Swift-UI/ContentView.swift
+++ Swift-UI/ContentView.swift
@@ -7,30 +7,26 @@
 
 import SwiftUI
 
-
-
-struct BookMark : Identifiable {
-    
-    let id = UUID()
-    let name : String
-    let icon : String
-    var item :[BookMark]?
-}
+import AVKit
 
 struct ContentView: View {
     
 //    @State private var color = Color.white
     
-    let item :[BookMark] = [.example1,.example2,.example3]
+
   
     var body: some View {
       
         
-        VStack{
-            List(item,children: \.item) { row in
+        VideoPlayer(player: AVPlayer(url: URL(string: "https://bit.ly/swswift")!)){
+            
+            VStack{
                 
-                Image(systemName: row.icon)
-                Text(row.name)
+                Text("Watermark")
+                    .font(.caption)
+                    .foregroundStyle(.white)
+                    .background(Color.black.opacity(0.7))
+                    .clipShape(Capsule())
             }
         }
       
@@ -42,19 +38,3 @@ struct ContentView: View {
 }
 
 
-extension BookMark {
-    static let apple = BookMark(name: "Apple", icon: "1.circle" )
-    static let bbc = BookMark(name: "BBC", icon: "square.and.pencil")
-    static let swift = BookMark(name: "Swift", icon: "bolt.fill")
-    static let twitter = BookMark(name: "Twitter", icon: "mic")
-    
-    static let example1 = BookMark(name: "Favorites", icon: "star", item: [
-        BookMark.apple,BookMark.bbc,BookMark.swift,BookMark.twitter
-    ])
-    static let example2 = BookMark(name: "Recent", icon: "star", item: [
-        BookMark.apple,BookMark.bbc,BookMark.swift,BookMark.twitter
-    ])
-    static let example3 = BookMark(name: "Recomended", icon: "star", item: [
-        BookMark.apple,BookMark.bbc,BookMark.swift,BookMark.twitter
-    ])
-}
