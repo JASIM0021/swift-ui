878159cd560701cdf23fd89053a7bbda9fc761ff
jasim0021 <skjasimuddin9064@gmail.com>
2024-09-15 18:39:05 +0530

    sync animation

--- Swift-UI/ContentView.swift
+++ Swift-UI/ContentView.swift
@@ -8,25 +8,41 @@
 import SwiftUI
 
 import AVKit
-
 struct ContentView: View {
     
-//    @State private var color = Color.white
+    @State private var isFlipped = false
     
+    @Namespace private var animation
 
   
     var body: some View {
       
         
-        VideoPlayer(player: AVPlayer(url: URL(string: "https://bit.ly/swswift")!)){
-            
-            VStack{
+     
+        VStack{
+            if isFlipped {
                 
-                Text("Watermark")
-                    .font(.caption)
-                    .foregroundStyle(.white)
-                    .background(Color.black.opacity(0.7))
-                    .clipShape(Capsule())
+                Circle()
+                    .fill(Color.red)
+                    .frame(width: 44,height: 44)
+                    .matchedGeometryEffect(id: "shape", in: animation)
+                Text("Jasim0021")
+                    .font(.headline)
+                    .matchedGeometryEffect(id: "text", in: animation)
+            }else{
+                Text("Jasim0021")
+                    .font(.headline)
+                    .matchedGeometryEffect(id: "text", in: animation)
+                Circle()
+                    .fill(Color.red)
+                    .frame(width: 44,height: 44)
+                    .matchedGeometryEffect(id: "shape", in: animation)
+            }
+            
+        }
+        .onTapGesture {
+            withAnimation{
+                isFlipped.toggle()
             }
         }
       
