91d86168cfb8738e85a078bb9b9f52322fb08782
jasim0021 <skjasimuddin9064@gmail.com>
2024-09-15 13:31:07 +0530

    mapview in swiftui

--- Swift-UI/ContentView.swift
+++ Swift-UI/ContentView.swift
@@ -6,14 +6,14 @@
 //
 
 import SwiftUI
-
+import MapKit
 struct ContentView: View {
+    
+    @State private var region = MKCoordinateRegion(center:CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+           
+            Map(coordinateRegion: $region)
         }
         .padding()
     }
