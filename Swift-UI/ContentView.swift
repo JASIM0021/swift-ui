62e17e5a621a0d5d500dcd8ff6c6a00118d74ee6
jasim0021 <skjasimuddin9064@gmail.com>
2024-09-15 14:11:25 +0530

    color picker added

--- Swift-UI/ContentView.swift
+++ Swift-UI/ContentView.swift
@@ -8,42 +8,23 @@
 import SwiftUI
 import SpriteKit
 
-class GameScene : SKScene {
-    
-    override func didMove(to view: SKView) {
-        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
-        
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else {
-            return
-        }
-        let location = touch.location(in: self)
-        
-        let box = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
-        
-        box.position = location
-        
-        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
-        addChild(box)
-    }
-}
 
 struct ContentView: View {
     
-    var scene : SKScene {
-        let scene  = GameScene()
-        scene.size = CGSize(width: 300, height: 400)
-        scene.scaleMode = .fill
-         return scene
-    }
+    @State private var color = Color.white
+  
     var body: some View {
-        VStack {
-             
-          SpriteView(scene: scene)
+      
+        ZStack{
+            Color(color)
+            VStack {
+                
+                ColorPicker("Set the Background Color", selection: $color)
+                    .frame(height: 200)
+            }
+//            .background(color)
+            .padding()
         }
-        .padding()
     }
 }
 
