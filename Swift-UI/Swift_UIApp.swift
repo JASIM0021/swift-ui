62b4d7dc4a5e158bc879d4122e590da78145c4e5
jasim0021 <skjasimuddin9064@gmail.com>
2024-09-15 13:19:16 +0530

    Initial Commit

---
+++ Swift-UI/Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UIApp.swift
+//  Swift-UI
+//
+//  Created by Sk Jasimuddin on 15/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
