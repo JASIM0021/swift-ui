6b7156ac922108dd8ba1f92ff99511f8144425c1
jasim0021 <skjasimuddin9064@gmail.com>
2024-09-15 17:58:45 +0530

    collaps view added

--- Swift-UI/ContentView.swift
+++ Swift-UI/ContentView.swift
@@ -6,28 +6,55 @@
 //
 
 import SwiftUI
-import SpriteKit
 
+
+
+struct BookMark : Identifiable {
+    
+    let id = UUID()
+    let name : String
+    let icon : String
+    var item :[BookMark]?
+}
 
 struct ContentView: View {
     
-    @State private var color = Color.white
+//    @State private var color = Color.white
+    
+    let item :[BookMark] = [.example1,.example2,.example3]
   
     var body: some View {
       
-        ZStack{
-            Color(color)
-            VStack {
+        
+        VStack{
+            List(item,children: \.item) { row in
                 
-                ColorPicker("Set the Background Color", selection: $color)
-                    .frame(height: 200)
+                Image(systemName: row.icon)
+                Text(row.name)
             }
-//            .background(color)
-            .padding()
         }
+      
     }
 }
 
 #Preview {
     ContentView()
+}
+
+
+extension BookMark {
+    static let apple = BookMark(name: "Apple", icon: "1.circle" )
+    static let bbc = BookMark(name: "BBC", icon: "square.and.pencil")
+    static let swift = BookMark(name: "Swift", icon: "bolt.fill")
+    static let twitter = BookMark(name: "Twitter", icon: "mic")
+    
+    static let example1 = BookMark(name: "Favorites", icon: "star", item: [
+        BookMark.apple,BookMark.bbc,BookMark.swift,BookMark.twitter
+    ])
+    static let example2 = BookMark(name: "Recent", icon: "star", item: [
+        BookMark.apple,BookMark.bbc,BookMark.swift,BookMark.twitter
+    ])
+    static let example3 = BookMark(name: "Recomended", icon: "star", item: [
+        BookMark.apple,BookMark.bbc,BookMark.swift,BookMark.twitter
+    ])
 }
